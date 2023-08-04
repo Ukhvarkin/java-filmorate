8d994703e8d7c8be4b5594b0f6c36c9cf396d437
Neo <djmx97@yandex.ru>
2023-08-04 16:12:13 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/MpaDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/MpaDaoImpl.java
@@ -8,30 +8,31 @@ import ru.yandex.practicum.filmorate.storage.dao.MpaDao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
 @Component
 public class MpaDaoImpl implements MpaDao {
-  private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-  public MpaDaoImpl(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
-  }
+    public MpaDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-  @Override
-  public List<Mpa> findAll() {
-    String sql = "SELECT * FROM mpa";
-    return jdbcTemplate.query(sql, this::makeMpa);
-  }
+    @Override
+    public List<Mpa> findAll() {
+        String sql = "SELECT * FROM mpa";
+        return jdbcTemplate.query(sql, this::makeMpa);
+    }
 
-  public Mpa findMpaServiceById(int id) {
-    String sql = "SELECT * FROM mpa WHERE mpa_id = ?";
-      return jdbcTemplate.queryForObject(sql, this::makeMpa, id);
-  }
+    public Mpa findMpaServiceById(int id) {
+        String sql = "SELECT * FROM mpa WHERE mpa_id = ?";
+        return jdbcTemplate.queryForObject(sql, this::makeMpa, id);
+    }
 
-  private Mpa makeMpa(ResultSet rs, int rowNum) throws SQLException {
-    return Mpa.builder()
-            .id(rs.getInt("mpa_id"))
-            .name(rs.getString("name"))
-            .description(rs.getString("description"))
-            .build();
-  }
+    private Mpa makeMpa(ResultSet rs, int rowNum) throws SQLException {
+        return Mpa.builder()
+                .id(rs.getInt("mpa_id"))
+                .name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .build();
+    }
 }
